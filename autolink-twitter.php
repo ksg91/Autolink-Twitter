897301879d6410887e9a7edaa4711ab9674123b1
Kishan Gor <admin@ksg91.com>
2012-01-11 11:52:05 +0530

    mentions replacement done

--- autolink-twitter.php
+++ autolink-twitter.php
@@ -8,4 +8,10 @@ Author: Kishan Gor
 Author URI: http://ksg91.com
 License: GPL2
 */
+function autoLinkTwitter($contents)
+{
+  //links mentions to the twitter profile
+  $contents=preg_replace('/(\s)@([a-zA-Z]+)/',' <a href="http://twitter.com/\2">@\2</a>',$contents);
+  return $contents;
+}
 ?>
