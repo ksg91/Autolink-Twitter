7d5455ba19be151acb74dcd8f963c272bb80f776
Kishan Gor <admin@ksg91.com>
2012-01-11 12:16:42 +0530

    fixed regex and registered filter

--- autolink-twitter.php
+++ autolink-twitter.php
@@ -3,7 +3,7 @@
 Plugin Name: Autolink Twitter
 Plugin URI: http://ksg91.com
 Description: A simple plugin for wordpress, which automatically link mentions in post or page to twitter profiles and hashtags to search link. 
-Version: 0.4
+Version: 0.5
 Author: Kishan Gor
 Author URI: http://ksg91.com
 License: GPL2
@@ -11,10 +11,10 @@ License: GPL2
 function autoLinkTwitter($contents)
 {
   //links mentions to the twitter profile
-  $contents=preg_replace('/(\s)@([a-zA-Z]+)/',' <a href="http://twitter.com/\2">@\2</a>',$contents);
+  $contents=preg_replace('/(\s)@([a-zA-Z0-9]+)/',' <a href="http://twitter.com/\2">@\2</a>',$contents);
   //links hashtags to the twtitter profile
-  $contents=preg_replace('/(\s)#([a-zA-Z]+)/',' <a href="http://twitter.com/search?q=%23\2">#\2</a>',$contents);
+  $contents=preg_replace('/(\s)#([a-zA-Z0-9]+)/',' <a href="http://twitter.com/search?q=%23\2">#\2</a>',$contents);
   return $contents;
 }
-
+add_filter( 'the_content', 'autoLinkTwitter' );
 ?>
