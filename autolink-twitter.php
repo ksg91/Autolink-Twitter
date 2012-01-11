9f9ba9cf91a23c4ed80d9aa810c99b01b2ca55c6
Kishan Gor <admin@ksg91.com>
2012-01-11 11:13:19 +0530

    Created Plugin file

---
+++ autolink-twitter.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Autolink Twitter
+Plugin URI: http://ksg91.com
+Description: A simple plugin for wordpress, which automatically link mentions in post or page to twitter profiles and hashtags to search link. 
+Version: 0.0
+Author: Kishan Gor
+Author URI: http://ksg91.com
+License: GPL2
+*/
+?>
